1ea8724cd62f7385a72f52b3394cc26fb92c6c10
maribelvm <m.velasquezmoscoso@gmail.com>
2017-10-16 14:48:14 -0700

    react-bootstrap

--- src/PhoneValidation.js
+++ src/PhoneValidation.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
 	BrowserRouter,
 	Route,
@@ -6,8 +6,12 @@ import {
 	NavLink,
 	Redirect
 } from 'react-router-dom'
-import './App.css';
+import './PhoneValidation.css';
 import phone from "./phone.png";
+import {
+	Button,
+} from 'react-materialize';
+import { Grid, Row, Col, Form, FormGroup, InputGroup, FormControl, Image } from 'react-bootstrap';
 
 // const PhoneValidation = ({ model }) => {
 //   return (
@@ -21,7 +25,8 @@ class PhoneValidation extends React.Component {
 			active: false
 		}
 	}
-	number(e) {
+
+		number(e) {
 		if (e.target.value.length == 9) {
 			this.setState({
 				active: true
@@ -35,33 +40,46 @@ class PhoneValidation extends React.Component {
 
 	render() {
 		return (
-			<div className="text-center">
-				<div className="pt-63 content-center text-center">
-					<img src={phone} className="w-40" alt="" />
-					<h5 className="less-margin rem-1"><strong>Para comenzar validemos tu número</strong></h5>
-					<span className="fs-12">Recibirás un SMS con un código de validación</span>
-					<input type="number" onChange={(e) => this.number(e)} className="input-yellow-bottom text-purple-dark text-center" placeholder="959666666" />
-					{/* <input type="number" id="numMobile" onChange={(e)=>this.getNumber(e)} className="form-control inp-l" placeholder="959666666" required="" /> */}
-					<br />
-					<input type="checkbox" id="terms" />
-					<label for="terms">Acepto los <span className="text-turquoise">términos y condiciones</span></label><br/>
-					{
-					this.state.active ?
-						<a href="#" className="button-register bg-yellow"><span className="text-uppercase text-white">continuar</span><br /></a>
-						:
-						<a href="#" className="button-register bg-gray-light"><span className="text-uppercase text-white">continuar</span><br /></a>
-				}
-				</div>
-				
 
-				{/* {
+			<Grid className="text-center">
+				<Row className="show-grid">
+					<Col xs={10} xsOffset={1} sm={8} smOffset={2} md={6} mdOffset={3} lg={4} lgOffset={4}><center><Image src={phone} /></center></Col>
+				</Row>
+				<Row className="show-grid">
+					<Col xs={10} xsOffset={1} sm={8} smOffset={2} md={6} mdOffset={3} lg={4} lgOffset={4}><h5 >Para comenzar validemos tu número</h5></Col>
+				</Row>
+				<Row className="show-grid">
+					<Col xs={10} xsOffset={1} sm={8} smOffset={2} md={6} mdOffset={3} lg={4} lgOffset={4}><p>Recibirás un SMS con un código de validación</p></Col>
+				</Row>
+				<Form>
+					<Row className="show-grid">
+						<Col xs={10} xsOffset={1} sm={8} smOffset={2} md={6} mdOffset={3} lg={4} lgOffset={4}>
+							<FormGroup controlId="formValidationSuccess4" validationState="success">
+								<InputGroup>
+
+									<FormControl onChange={(e) => this.number(e)}type="number" placeholder="999999999"  />
+									<FormControl type="checkbox"/>
+									{/* <input type="checkbox" id="terms" /> */}
+									<label for="terms">Acepto los <span className="text-turquoise">términos y condiciones</span></label><br />
+								</InputGroup>
+
+							</FormGroup>
+						</Col>
+					</Row>
+					<Row className="show-grid">
+						<Col xs={10} xsOffset={1} sm={8} smOffset={2} md={6} mdOffset={3} lg={4} lgOffset={4}>
+							{
 								this.state.active ?
-									<NavLink to={"/code-validation"} className="block button-register bg-yellow" >Next</NavLink>
+									<NavLink to={"/code-validation"} className="block button-register bg-yellow">CONTINUAR</NavLink>
 									:
-									<NavLink to={"/code-validation"} className="block button-register bg-gray-light" >Next</NavLink>
-							} */}
-			</div>
-		
+									<NavLink to={"/code-validation"} className="button-register bg-gray-light text-white">CONTINUAR</NavLink>
+							}
+						</Col>
+					</Row>
+				</Form>
+
+			</Grid>
+			
 		)
 	}
 
