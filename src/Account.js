2b6418aec8c87f5dab316ede66d15f60f958df33
AQP20171679 <alejandraadriantejada@gmail.com>
2017-10-16 09:20:03 -0500

    Primera parte de la vista

--- src/Account.js
+++ src/Account.js
@@ -6,11 +6,23 @@ import {
 	NavLink,
 	Redirect
 } from 'react-router-dom'
-import './App.css';
+import './Account.css';
+import {Input} from  'react-materialize';
+import {Grid, Row, Col} from  'react-bootstrap';
 
 const Account = ({ model }) => {
   return (
-    <h2>Account</h2>
+		<div className='text-center account'>
+			<Row className='hello'>
+				<Col xs={10} xsOffset={1}  sm={8} smOffset={2} md={6} mdOffset={3} lg={4} lgOffset={4}>
+					<img className='img-happy' src='http://174.138.48.60:3000/img/icons/happy-face.png'/>
+					<h5 className='text-white'>Hola</h5>
+					<span className='text-turquoise'> <img src="http://174.138.48.60:3000/img/icons/eye.png" className="eye w-7"/>Mostrar saldo</span>
+				</Col>
+			</Row>
+			<Row></Row>
+			<Row></Row>
+		</div>
   );
 };
 
