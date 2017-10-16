2f1916f153ed03c0f48c11676843f0f5a583d603
gabiprds93 <gabiprds.93@gmail.com>
2017-10-16 00:17:42 -0500

    Maquetacion pantalla password-card

--- src/PasswordCard.js
+++ src/PasswordCard.js
@@ -5,12 +5,42 @@ import {
 	Switch,
 	NavLink,
 	Redirect
-} from 'react-router-dom'
+} from 'react-router-dom';
+import {Grid, Row, Col, Form, FormGroup, InputGroup, FormControl, Button} from 'react-bootstrap';
 import './App.css';
+import bcpCard from './images/bcpCard.png';
 
 const PasswordCard = ({ model }) => {
   return (
-    <h2>PasswordCard</h2>
+    <Grid className="text-center">
+			<Row className="show-grid">
+				<Col xs={4} xsOffset={4}><img src={bcpCard}/></Col>
+			</Row>
+			<Row className="show-grid">
+				<Col xs={4} xsOffset={4}><h4>Ingresa la clave de tu tarjeta</h4></Col>
+			</Row>
+			<Row className="show-grid">
+				<Col xs={4} xsOffset={4}><p>Tarjeta</p></Col>
+			</Row>
+			<Form>
+				<Row className="show-grid">
+					<Col xs={4} xsOffset={4}>
+						<FormGroup controlId="formValidationSuccess4" validationState="success">
+							<InputGroup>
+								<InputGroup.Addon><i class="fa fa-lock" aria-hidden="true"></i></InputGroup.Addon>
+								<FormControl type="text" />
+							</InputGroup>
+							<FormControl.Feedback />
+						</FormGroup>
+					</Col>							
+				</Row>
+				<Row className="show-grid">
+					<Col xs={4} xsOffset={4}>
+						<Button type="submit">REGISTRAR</Button>
+					</Col>
+				</Row>	
+			</Form>
+		</Grid>
   );
 };
 
