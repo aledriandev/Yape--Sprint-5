bdfa42e1f127612ee167c5ceee8127bf0e0adbbf
luisaGonzales <luisagc711@gmail.com>
2017-10-16 15:59:48 -0500

    Register completo

--- src/YapeModel.js
+++ src/YapeModel.js
@@ -12,6 +12,7 @@ class YapeModel {
       cardYear: "",
       passwordCard: '',
     }
+    this.activeNextRegisterCard = false;
     this.timer = null;
   }
 	subscribe (render) {
@@ -35,11 +36,38 @@ class YapeModel {
     if (!isNaN(e.target.value))
     {
       this.user.numberCard = e.target.value;
+      console.log(this.user.numberCard.length)
+      this.isCompleteRegisterCard();
       this.notify();
-      console.log(this.user.numberCard);
     }
   }
-  
+  getCardMonth(e)
+  {
+    if(!isNaN(e.target.value))
+    {
+      this.user.cardMonth = e.target.value;
+      console.log(this.user.cardMonth.length)
+      this.isCompleteRegisterCard();
+      this.notify();
+    }
+  }
+  getCardYear(e)
+  {
+    if(!isNaN(e.target.value))
+    {
+      this.user.cardYear = e.target.value;
+      console.log(this.user.cardYear)
+      this.isCompleteRegisterCard();
+      this.notify();
+    }
+  }
+  isCompleteRegisterCard()
+  {
+    if((this.user.numberCard.length == 16) && (this.user.cardMonth.length == 2) && (this.user.cardYear.length == 2))
+    {
+      this.activeNextRegisterCard = true;
+    }
+  }
 }
 
 export default YapeModel;
