29480df3e89cb3a1b5827e88b2a007dfb12dc257
gabiprds93 <gabiprds.93@gmail.com>
2017-10-16 01:09:40 -0500

    Mejorando maquetacion pantalla password-card

--- src/PasswordCard.js
+++ src/PasswordCard.js
@@ -6,36 +6,36 @@ import {
 	NavLink,
 	Redirect
 } from 'react-router-dom';
-import {Grid, Row, Col, Form, FormGroup, InputGroup, FormControl, Button} from 'react-bootstrap';
-import './App.css';
+import {Grid, Row, Col, Form, FormGroup, InputGroup, FormControl, Image, Button} from 'react-bootstrap';
+import './PasswordCard.css';
 import bcpCard from './images/bcpCard.png';
 
 const PasswordCard = ({ model }) => {
   return (
     <Grid className="text-center">
 			<Row className="show-grid">
-				<Col xs={4} xsOffset={4}><img src={bcpCard}/></Col>
+				<Col xs={10} xsOffset={1}  sm={8} smOffset={2} md={6} mdOffset={3} lg={4} lgOffset={4}><center><Image src={bcpCard} /></center></Col>
 			</Row>
 			<Row className="show-grid">
-				<Col xs={4} xsOffset={4}><h4>Ingresa la clave de tu tarjeta</h4></Col>
+				<Col xs={10} xsOffset={1} sm={8} smOffset={2} md={6} mdOffset={3} lg={4} lgOffset={4}><h4>Ingresa la clave de tu tarjeta</h4></Col>
 			</Row>
 			<Row className="show-grid">
-				<Col xs={4} xsOffset={4}><p>Tarjeta</p></Col>
+				<Col xs={10} xsOffset={1} sm={8} smOffset={2} md={6} mdOffset={3} lg={4} lgOffset={4}><p>Tarjeta</p></Col>
 			</Row>
 			<Form>
 				<Row className="show-grid">
-					<Col xs={4} xsOffset={4}>
+					<Col xs={10} xsOffset={1} sm={8} smOffset={2} md={6} mdOffset={3} lg={4} lgOffset={4}>
 						<FormGroup controlId="formValidationSuccess4" validationState="success">
 							<InputGroup>
 								<InputGroup.Addon><i class="fa fa-lock" aria-hidden="true"></i></InputGroup.Addon>
-								<FormControl type="text" />
+								<FormControl type="password" placeholder="* * * *" maxLength="4"/>
 							</InputGroup>
 							<FormControl.Feedback />
 						</FormGroup>
 					</Col>							
 				</Row>
 				<Row className="show-grid">
-					<Col xs={4} xsOffset={4}>
+					<Col xs={10} xsOffset={1} sm={8} smOffset={2} md={6} mdOffset={3} lg={4} lgOffset={4}>
 						<Button type="submit">REGISTRAR</Button>
 					</Col>
 				</Row>	
