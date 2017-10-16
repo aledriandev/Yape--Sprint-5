306a6abe9f6b5c4d4b07ae3d641c76ab1754df66
milagrosMndoza <milagrosmndoza@gmail.com>
2017-10-16 16:27:19 -0500

    bootstrap 4

--- src/Register.js
+++ src/Register.js
@@ -1,43 +1,66 @@
 import React, {Component} from 'react';
-import {
-	BrowserRouter,
-	Route,
-	Switch,
-	NavLink,
-	Redirect
-} from 'react-router-dom'
-import './App.css';
-import {Input, Button} from  'react-materialize';
-import {Carousel, CarouselCaption, CarouselItem} from  'react-bootstrap'
+import { UncontrolledCarousel } from 'reactstrap';
+import './Register.css';
+// import {Input, Button} from  'react-materialize';
+
 // import Carousel from '../src/Carousel';
 
+class Register extends Component{
+constructor(props){
+  super (props)
 
-const Register = ({ model }) => {
-  return (
-		<Carousel>
-    <Carousel.Item>
-      <img width={900} height={500} alt="900x500" src="/images/group-people.png"/>
-      <Carousel.Caption>
-        <h3>First slide label</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img width={900} height={500} alt="900x500" src="/images/happy-person.png"/>
-      <Carousel.Caption>
-        <h3>Second slide label</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img width={900} height={500} alt="900x500" src="/images/icon-people.png"/>
-      <Carousel.Caption>
-        <h3>Third slide label</h3>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
-	)
-};
+}
+render (){
+  const model = this.props;
+  const items = [
+    {
+      src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+      altText: 'Slide 1',
+      caption: 'Slide 1'
+    },
+    {
+      src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+      altText: 'Slide 2',
+      caption: 'Slide 2'
+    },
+    {
+      src: '"/img/group-people.png"',
+      altText: 'Slide 3',
+      caption: 'Slide 3'
+    }
+  ];
+  return (    
+   <UncontrolledCarousel items={items} />
+  )
+}
+}
 
 export default Register;
+
+
+{/* //<Carousel className="carousel-slide">
+//   <Carousel.Item>
+//     <img className="img- img-responsive" alt="900x500" src={"/img/group-people.png"}/>
+//     <Carousel.Caption>
+//       <h3>First slide label</h3>
+//       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//     </Carousel.Caption>
+//   </Carousel.Item>
+//   <div className="text-center">
+//   <Carousel.Item>
+    
+//     <img className="img-carousel img-responsive" alt="900x500" src={"/img/happy-person.png"}/>
+//     <Carousel.Caption>
+//       <h3>Second slide label</h3>
+//       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+//     </Carousel.Caption>
+//   </Carousel.Item>
+//   </div>
+//   <Carousel.Item>
+//     <img className="img-carousel img-responsive" alt="900x500" src={"/img/icon-people.png"}/>
+//     <Carousel.Caption>
+//       <h3>Third slide label</h3>
+//       <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+//     </Carousel.Caption>
+//   </Carousel.Item>
+// </Carousel> */}
