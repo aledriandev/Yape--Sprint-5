22efe1cf545d586515f8cfaa6efd26ab25241cf9
gabiprds93 <gabiprds.93@gmail.com>
2017-10-17 17:12:47 -0500

    Arreglando css

--- src/ReceivePayment.js
+++ src/ReceivePayment.js
@@ -9,13 +9,13 @@ const ReceivePayment = ({ model }) => {
   return (
     <Grid className="text-center receivePaymentGrid">
 			<Row className="show-grid">
-				<Col xs={12} sm={2} md={2} lg={2}>
+				<Col xs={2} sm={2} md={2} lg={2}>
 					<NavLink to={"/account"}><span className="fa fa-angle-left fa-2x"></span></NavLink>
 				</Col>
 			</Row>
 			<Row className="show-grid">
 				<Col xs={8} xsOffset={2} sm={8} smOffset={2} md={6} mdOffset={3} lg={6} lgOffset={3}>
-					<h5 className="titlePasswordCard" >Presenta este código para recibir pagos</h5>
+					<h5 className="titleReceivePayment" >Presenta este código para recibir pagos</h5>
 				</Col>
 			</Row>
 			<Row className="show-grid">
